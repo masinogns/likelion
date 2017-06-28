d829eb14ae9be785b129b0f38043c5b0a88f2e3f
b.kihoon <masinogns@gmail.com>
2017-06-28 20:22:05 +0900

    로그아웃 에러 해결 : link to ~~~ method :delete ==> get으로 하니까 로그아웃도 됨.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # devise_for :users do
+  # get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
+  devise_scope :user do
+   get '/users/sign_out' => 'devise/sessions#destroy'
+ end
+
   get '/posts/new' => 'posts#new'
   post '/posts/create' => 'posts#create'
 
---
+++ db/migrate/20170628104331_add_post_to_count.rb
@@ -0,0 +1,5 @@
+class AddPostToCount < ActiveRecord::Migration[5.1]
+  def change
+    add_column :posts, :count, :integer
+  end
+end
